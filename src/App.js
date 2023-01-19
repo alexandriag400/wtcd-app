d98cd1f328bc10504d0c8af9cf0f0873088cead3
Alex Gordon <alexandriag400@gmail.com>
2023-01-18 20:18:46 -0500

    added some content

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
+import Navbar from './components/Navbar/Navbar'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar></Navbar>
+      <h1>Hello</h1>
+      <div class = "div">
+        <h2>Cats are the best</h2>
+        <p>(If you didn't know)</p>
+        <h6>very tiny</h6>
+      </div>
+      <img src="http://pixelartmaker-data-78746291193.nyc3.digitaloceanspaces.com/image/ba79ccaf05eaf17.png" alt="Kitty" width="460" height="345"></img>
+      <img src="https://purepng.com/public/uploads/medium/red-pixel-heart-vmx.png" alt="heart" width="50" height="50"></img>
     </div>
   );
 }
-
 export default App;
